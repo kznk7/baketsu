5f234e6c0becc086a2b5b0be161bb8ea584ea3bc
kznk7 <e1b18069@st.oit.ac.jp>
2020-10-06 16:20:05 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/group11/baketsu/BaketsuApplication.java
@@ -0,0 +1,13 @@
+package oit.is.group11.baketsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaketsuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaketsuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/group11/baketsu/BaketsuApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.group11.baketsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaketsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
